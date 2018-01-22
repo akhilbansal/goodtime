98bbb546151ebe538fde80eb38741a2a59acd25b
Akhil <akhil.bansal@gmail.com>
2018-01-22 15:00:36 -0600

    Update alexa_skill.py

--- alexa_skill.py
+++ alexa_skill.py
@@ -2,7 +2,7 @@ import datetime
 from datetime import date
 
 bday = date(2018, 3, 31)
-dates = ['2017-12-25', '2018-01-01', '2018-01-15', '2018-02-19', '2016-03-31']
+dates = ['2017-12-25', '2018-01-01', '2018-01-15', '2018-02-19', '2018-03-31']
 
 def convert_to_date():
     lst = [];
@@ -24,6 +24,12 @@ def get_date_by_year(year):
     temp = sorted(x for x in lst if x.year == year)
     return temp
 
+def get_date_by_year_month(year, month):
+    lst = convert_to_date()
+    temp = sorted(x for x in lst if x.year == year and x.month == month)
+    print(temp)
+    return temp
+
 def lambda_handler(event, context):
     if event['request']['type'] == "IntentRequest":
         return on_intent(event['request'], event['session'])
@@ -36,7 +42,7 @@ def on_intent(intent_request, session):
     if intent_name == "good_news":
         return conversation("test", "this is what alexa will speak")
     elif intent_name == "good_news_by_month":
-        return conversation("test", "in the month of" + dateToString(get_date_by_year(2018)))
+        return conversation("test", "in the month of " + dateToString(get_date_by_year_month(2018,3)))
     elif intent_name == "devicecode":
         return "not yet implemented."
     else:
@@ -70,6 +76,10 @@ def conversation(title, alexa_response):
 
 
 def aa():
+
+    month_name = "jul"
+    print(datetime.datetime.strptime(month_name, '%b').month)
+
     slots = {}
     slots['name']='AMAZON.Month'
     slots['value'] = 'January'
